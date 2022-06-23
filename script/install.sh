c321ae93499e85466163729e17ae6d015439f2c7
le0tk0k <le0tk0k@gmail.com>
2022-06-23 20:58:11 +0900

    [script]: fix for shellcheck

--- script/install.sh
+++ script/install.sh
@@ -3,5 +3,5 @@
 set -eu
 
 # todo set the variable DOTFILES_DIR and link other config files.
-ln -snfv $HOME/dotfiles/config/tmux/tmux.conf $HOME/.config/tmux/tmux.conf
-ln -snfv $HOME/dotfiles/config/starship/starship.toml $HOME/.config/starship.toml
+ln -snfv "$HOME/dotfiles/config/tmux/tmux.conf" "$HOME/.config/tmux/tmux.conf"
+ln -snfv "$HOME/dotfiles/config/starship/starship.toml" "$HOME/.config/starship.toml"
