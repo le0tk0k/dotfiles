9774ba67334277e4245fb89b529647a2c03b0fd8
le0tk0k <le0tk0k@gmail.com>
2022-06-25 19:42:25 +0900

    [script]: add new links

--- script/install.sh
+++ script/install.sh
@@ -5,3 +5,6 @@ set -eu
 # todo set the variable DOTFILES_DIR and link other config files.
 ln -snfv "$HOME/dotfiles/config/tmux/tmux.conf" "$HOME/.config/tmux/tmux.conf"
 ln -snfv "$HOME/dotfiles/config/starship/starship.toml" "$HOME/.config/starship.toml"
+ln -snfv "$HOME/dotfiles/config/git/.gitconfig" "$HOME/.gitconfig"
+ln -snfv "$HOME/dotfiles/config/git/.gitignore_global" "$HOME/.gitignore_global"
+ln -snfv "$HOME/dotfiles/config/hammerspoon/init.lua" "$HOME/.hammerspoon/init.lua"
